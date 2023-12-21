a134f5ffe82439c2734966a33af912cb1adad2ab
Alejandro <ajorgenmarten35@gmail.com>
2023-12-20 19:40:24 -0800

    startup project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './assets/index.css'
+import { Navbar } from './components/navbar'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <div className='container mx-auto'>
+      <h1>Hola mundo</h1>
+      <button className='btn btn-primary'>Boton primario</button>
+    </div>
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
 
